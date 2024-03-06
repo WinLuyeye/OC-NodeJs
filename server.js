37ebb55ff68414a1d1f4717f0029499e367dcb92
Winner Luyeye <winnerluyeye1@gmail.com>
2024-03-06 20:58:23 +0100

    Code improvement and error handling

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
-const http = require('http'); //Creation du serveur
-const app = require('./app'); //Importation de express
+const http = require('http');
+const app = require('./app');
 
-const server = http.createServer(app); // app etant une fonction qui retournera une reponse et une requette
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-app.set['PORT', process.env.PORT || 3000] // Definir un port de demarage de notre app(express)
-// Ici on demare notre serveur sur le 3000 mais "process.env.PORT : c'est pour dire a l'ordinateur que si le port 3000 est occupée qu'il nous donne n'importe quel port"
-server.listen(process.env.PORT || 3000); 
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
