5c73892a295539ec4890d8e55916f65b07c7a3c5
Winner Luyeye <winnerluyeye1@gmail.com>
2024-03-06 20:10:36 +0100

    Installation of express, Framework node to allow us to create APIs

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express') //Creation et importation de express
+
+const app = express();
+
+
+module.exports = app; //Exportaer express pour lui rendre accessible ou importable a tous.
