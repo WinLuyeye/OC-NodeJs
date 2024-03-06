9f25967106fbf34dc05aa46238084482103838a8
Winner Luyeye <winnerluyeye1@gmail.com>
2024-03-06 20:35:40 +0100

    Import express(App) and set a startup port

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const http = require('http'); //Creation du serveur
+const app = require('./app'); //Importation de express
 
-//Le serveur doit avoir deux argument 1. La requette et 2. La reponse ; ex: req : Requette ; res : reponse.
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !'); // Exemple du message de la reponse
-});
+const server = http.createServer(app); // app etant une fonction qui retournera une reponse et une requette
 
+app.set['PORT', process.env.PORT || 3000] // 
 // Ici on demare notre serveur sur le 3000 mais "process.env.PORT : c'est pour dire a l'ordinateur que si le port 3000 est occupée qu'il nous donne n'importe quel port"
 server.listen(process.env.PORT || 3000); 
